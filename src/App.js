a9ed111cc915f047d6724f804f28bd42064bbab8
unknown <assegidassefa7@gmail.com>
2023-05-05 10:44:36 +0300

    add text-animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import TextSpan from "./TextSpan";
 function App() {
+  const sentence = "Hello Assegid".split("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {sentence.map((letter, index) => {
+        return <TextSpan key={index}>{letter}</TextSpan>;
+      })}
     </div>
   );
 }
